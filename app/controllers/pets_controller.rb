2228e36fb08b3cd3ea4ed8fd3eeabab58d08ef34
Monica Marziano <mmarziano1@gmail.com>
2019-05-26 21:19:42 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,9 +37,11 @@ class PetsController < ApplicationController
     
     @pet = Pet.find(params[:id])
     @pet.update(params["pets"])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Pet.create(name: params["owner"]["name"])
-    end
+    if params["owner"]["name"].empty?
+        @pet.owner = Owner.find(params[:pets][:owner_id]).first
+      else 
+        @pet.owner = Owner.create(name: params["owner"]["name"])
+      end
     redirect to "pets/#{@pet.id}"
   end
   
