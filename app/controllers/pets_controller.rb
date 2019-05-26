6b985436b58ede22f0ae882ff4add6b9867bc59e
Monica Marziano <mmarziano1@gmail.com>
2019-05-26 20:41:50 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,6 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do 
     binding.pry
      @pet = Pet.create(params[:pets])
+     owner = Owner.find(params["pets"]["owner_id"])
       if !params["owner"]["name"].empty?
         @pet.owner = Owner.create(name: params["owner"]["name"])
       end
