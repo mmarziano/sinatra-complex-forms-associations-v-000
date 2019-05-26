bc39d381a6ef061bdb0fa9a35f044bd9ab422f53
Monica Marziano <mmarziano1@gmail.com>
2019-05-26 21:11:38 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,8 @@ class PetsController < ApplicationController
     #binding.pry
      @pet = Pet.create(params[:pets])
       if params["owner"]["name"].empty?
-        @pet.owner = Owner.find(params[:pets][:owner_id]).first
+        @pet.owner = Pet.create(name: params["owner"]["name"])
+        #Owner.find(params[:pets][:owner_id]).first
       end
      binding.pry
     redirect to "pets/#{@pet.id}"
